993f2895462d11834761cf9325ad39fbc9b82ced
Augusto Estevão Monte <augusto-estevao@hotmail.com>
2023-05-19 22:04:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search)
-    console.log(data.Search)
   }
 
   useEffect(() => {
